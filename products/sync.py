6a0ab748122fac0100a83d7cc7d722203cc3ae12
David Oak <mr.davidoak@gmail.com>
2023-09-18 08:43:45 +0100

    allow all products ot be imported

--- products/sync.py
+++ products/sync.py
@@ -91,87 +91,95 @@ def import_new_products(request):
         "per_page": PER_PAGE,
     }
 
+    # Fetch the first page to get the total number of pages
     response = requests.get(product_url, params=params)
-    all_products = response.json()
-    total_products = len(all_products)
+    total_pages = int(response.headers.get('X-WP-TotalPages', 1))
 
-    for index, product in enumerate(all_products, start=1):
-        # Check if the product already exists in the database
-        if Product.objects.filter(product_id=product['id']).exists():
-            print(f"Product ID {product['id']} already exists. Skipping.")
-            continue
+    for page in range(1, total_pages + 1):
+        params['page'] = page
+        response = requests.get(product_url, params=params)
+        all_products = response.json()
+        total_products = len(all_products)
 
-        product_data, _ = build_product_data(product)
-        
-        # Attempt to create or update the product
-        try:
-            with transaction.atomic():
-                product_instance, created = Product.objects.update_or_create(product_id=product['id'], defaults=product_data)
-                
-                # Save meta data for the main product
-                for meta in product.get('meta_data', []):
-                    try:
-                        ProductMetaData.objects.update_or_create(
-                            product=product_instance,
-                            key=meta['key'],
-                            defaults={
-                                'value': meta['value'],
-                                'description': meta.get('description', '')
-                            }
-                        )
-                    except Exception as e:
-                        print(f"Error with Meta Key '{meta['key']}' for Product ID {product['id']}: {str(e)}")
-                        print(f"Meta Value: {meta['value']}")  # This line will print the value causing the issue
-                
-        except IntegrityError as e:
-            print(f"Integrity Error with Product ID {product['id']}: {str(e)}")
-            continue
-        except Exception as e:
-            print(f"General Error with Product ID {product['id']}: {str(e)}")
-            continue
+        for index, product in enumerate(all_products, start=1):
+            # Check if the product already exists in the database
+            if Product.objects.filter(product_id=product['id']).exists():
+                print(f"Product ID {product['id']} already exists. Skipping.")
+                continue
 
-        if created and product['type'] == 'variable':
-            variation_url = BASE_URL + f"products/{product['id']}/variations"
-            variation_params = {
-                "consumer_key": params["consumer_key"],
-                "consumer_secret": params["consumer_secret"]
-            }
-            variations_response = requests.get(variation_url, params=variation_params)
-            variations = variations_response.json()
+            product_data, _ = build_product_data(product)
             
-            for var_index, variation in enumerate(variations, start=1):
-                variation_data, _ = build_product_data(product, product_type='variation', variation=variation)
-                
-                # Attempt to create or update the variation
-                try:
-                    with transaction.atomic():
-                        variation_instance, var_created = Product.objects.update_or_create(product_id=variation['id'], defaults=variation_data)
-                        
-                        # Save meta data for the variation
-                        for meta in variation.get('meta_data', []):
-                            try:
-                                ProductMetaData.objects.update_or_create(
-                                    product=variation_instance,
-                                    key=meta['key'],
-                                    defaults={
-                                        'value': meta['value'],
-                                        'description': meta.get('description', '')
-                                    }
-                                )
-                            except Exception as e:
-                                print(f"Error with Meta Key '{meta['key']}' for Variation ID {variation['id']} (Product ID {product['id']}): {str(e)}")
-                                print(f"Meta Value: {meta['value']}")  # This line will print the value causing the issue
-                        
-                except IntegrityError as e:
-                    print(f"Integrity Error with Variation ID {variation['id']} for Product ID {product['id']}: {str(e)}")
-                    continue
-                except Exception as e:
-                    print(f"General Error with Variation ID {variation['id']} for Product ID {product['id']}: {str(e)}")
-                    continue
+            # Attempt to create or update the product
+            try:
+                with transaction.atomic():
+                    product_instance, created = Product.objects.update_or_create(product_id=product['id'], defaults=product_data)
+                    
+                    # Save meta data for the main product
+                    for meta in product.get('meta_data', []):
+                        try:
+                            ProductMetaData.objects.update_or_create(
+                                product=product_instance,
+                                key=meta['key'],
+                                defaults={
+                                    'value': meta['value'],
+                                    'description': meta.get('description', '')
+                                }
+                            )
+                        except Exception as e:
+                            print(f"Error with Meta Key '{meta['key']}' for Product ID {product['id']}: {str(e)}")
+                            print(f"Meta Value: {meta['value']}")  # This line will print the value causing the issue
+                    
+            except IntegrityError as e:
+                print(f"Integrity Error with Product ID {product['id']}: {str(e)}")
+                continue
+            except Exception as e:
+                print(f"General Error with Product ID {product['id']}: {str(e)}")
+                continue
 
-                print(f"Imported Variation {var_index}/{len(variations)} for Product {index}/{total_products}")
+            if created and product['type'] == 'variable':
+                variation_url = BASE_URL + f"products/{product['id']}/variations"
+                variation_params = {
+                    "consumer_key": params["consumer_key"],
+                    "consumer_secret": params["consumer_secret"]
+                }
+                variations_response = requests.get(variation_url, params=variation_params)
+                variations = variations_response.json()
                 
-        print(f"Product Import Progress: {index}/{total_products}")
+                for var_index, variation in enumerate(variations, start=1):
+                    variation_data, _ = build_product_data(product, product_type='variation', variation=variation)
+                    
+                    # Attempt to create or update the variation
+                    try:
+                        with transaction.atomic():
+                            variation_instance, var_created = Product.objects.update_or_create(product_id=variation['id'], defaults=variation_data)
+                            
+                            # Save meta data for the variation
+                            for meta in variation.get('meta_data', []):
+                                try:
+                                    ProductMetaData.objects.update_or_create(
+                                        product=variation_instance,
+                                        key=meta['key'],
+                                        defaults={
+                                            'value': meta['value'],
+                                            'description': meta.get('description', '')
+                                        }
+                                    )
+                                except Exception as e:
+                                    print(f"Error with Meta Key '{meta['key']}' for Variation ID {variation['id']} (Product ID {product['id']}): {str(e)}")
+                                    print(f"Meta Value: {meta['value']}")  # This line will print the value causing the issue
+                            
+                    except IntegrityError as e:
+                        print(f"Integrity Error with Variation ID {variation['id']} for Product ID {product['id']}: {str(e)}")
+                        continue
+                    except Exception as e:
+                        print(f"General Error with Variation ID {variation['id']} for Product ID {product['id']}: {str(e)}")
+                        continue
+
+                    print(f"Imported Variation {var_index}/{len(variations)} for Product {index}/{total_products}")
+                    
+            print(f"Product Import Progress for Page {page}: {index}/{total_products}")
+        print(f"Finished importing products from page {page}/{total_pages}")
+
 
 
 
